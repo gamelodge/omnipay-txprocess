078595059cec7210c36a5f0f160abd90dede7c46
dfrasee <rasee59@gmail.com>
2016-01-06 14:12:03 +0700

    valid card

--- src/Message/CardPurchaseRequest.php
+++ src/Message/CardPurchaseRequest.php
@@ -62,7 +62,7 @@ class CardPurchaseRequest extends AbstractRequest
         $data = parent::getData();
         $card = $this->getCard();
         $card->validate();
-        if ($this->LuhnCheckCardNumber($card->getNumber())) {
+        if (!$this->LuhnCheckCardNumber($card->getNumber())) {
             throw new InvalidCreditCardException('Card number is invalid');
         }
         $type = $card->getBrand()? $card->getBrand() : 'visa';
@@ -84,33 +84,23 @@ class CardPurchaseRequest extends AbstractRequest
     * give credit to the original author where possible.                      */
 
    function LuhnCheckCardNumber($number) {
-
-     // Strip any non-digits (useful for credit card numbers with spaces and hyphens)
-     $number=preg_replace('/\D/', '', $number);
-
-     // Set the string length and parity
-     $number_length=strlen($number);
-     $parity=$number_length % 2;
-
-     // Loop through each digit and do the maths
-     $total=0;
-     for ($i=0; $i<$number_length; $i++) {
-       $digit=$number[$i];
-       // Multiply alternate digits by two
-       if ($i % 2 == $parity) {
-         $digit*=2;
-         // If the sum is two digits, add them together (in effect)
-         if ($digit > 9) {
-           $digit-=9;
-         }
-       }
-       // Total up the digits
-       $total+=$digit;
-     }
-
-     // If the total mod 10 equals 0, the number is valid
-     return ($total % 10 == 0) ? TRUE : FALSE;
-
+        $cards = array(
+            "visa" => "(4\d{12}(?:\d{3})?)",
+            "amex" => "(3[47]\d{13})",
+            "jcb" => "(35[2-8][89]\d\d\d{10})",
+            "maestro" => "((?:5020|5038|6304|6579|6761)\d{12}(?:\d\d)?)",
+            "solo" => "((?:6334|6767)\d{12}(?:\d\d)?\d?)",
+            "mastercard" => "(5[1-5]\d{14})",
+            "switch" => "(?:(?:(?:4903|4905|4911|4936|6333|6759)\d{12})|(?:(?:564182|633110)\d{10})(\d\d)?\d?)",
+        );
+        $names = array("Visa", "American Express", "JCB", "Maestro", "Solo", "Mastercard", "Switch");
+        $matches = array();
+        $pattern = "#^(?:".implode("|", $cards).")$#";
+        $result = preg_match($pattern, str_replace(" ", "", $cc), $matches);
+        if($extra_check && $result > 0){
+            $result = (validatecard($cc))?1:0;
+        }
+        return ($result>0)?$names[sizeof($matches)-2]:false;
    }
 
 }
